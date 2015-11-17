7cb9c53735a77d59b23ada0fbe16c20a4114aa1d
Mathias Raacke <mathias@raacke.info>
2015-11-17 22:40:27 +0100

    added missing file

---
+++ src/Oocx.ACME.Common/ILog.cs
@@ -0,0 +1,13 @@
+﻿namespace Oocx.ACME.Common
+{
+    public interface ILog
+    {
+        void Verbose(string message, params object[] args);
+
+        void Info(string message, params object[] args);
+
+        void Warning(string message, params object[] args);
+
+        void Error(string message, params object[] args);
+    }
+}
