4f1abb1ed682a6cd4432ad3585837b8c0ede5c54
Mathias Raacke <mathias@raacke.info>
2015-11-19 22:21:17 +0100

    set permissions for acme challenge path

--- src/Oocx.ACME.IIS/IISChallengeService.cs
+++ src/Oocx.ACME.IIS/IISChallengeService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.Web.Administration;
 using static Oocx.ACME.Common.Log;
@@ -34,38 +36,55 @@ namespace Oocx.ACME.IIS
         public void AcceptChallenge(string domain, string token, string challengeJson)
         {
             Info($"IISChallengeService is accepting challenge with token {token} for domain {domain}");
-            var challengePath = Path.Combine(Path.GetTempPath(), "oocx.acme");
-            CreateDirectory(challengePath);
-            var wellKnownPath = Path.Combine(challengePath, ".well-known");
-            CreateDirectory(wellKnownPath);
-            var acmePath = Path.Combine(wellKnownPath, "acme");
-            CreateDirectory(acmePath);
-            var challengeFilePath = Path.Combine(acmePath, token);
-            File.WriteAllText(challengeFilePath, challengeJson);
-            var acmeWebConfigFilePath = Path.Combine(acmePath, "web.config");
-            File.WriteAllText(acmeWebConfigFilePath, AcmeWebConfigContents);
+            var wellKnownPath = CreateWellKnownDirectory(token, challengeJson);            
+            CreateIISVirtualDirectory(domain, wellKnownPath);
+        }
 
+        private static void AllowReadPermissionsForEveryone(string wellKnownPath)
+        {
+            //TODO instead of allowing read for everyone, only allow read for the IIS application pool account
+            var accessControl = File.GetAccessControl(wellKnownPath);
+            var everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+            var allowRead = new FileSystemAccessRule(everyone, FileSystemRights.Read, AccessControlType.Allow);
+            accessControl.AddAccessRule(allowRead);
+        }
+
+        private void CreateIISVirtualDirectory(string domain, string wellKnownPath)
+        {
             var site = GetSiteForDomain(domain);
             var root = site.Applications["/"];
             var wellKnownDir = root.VirtualDirectories.FirstOrDefault(d => "/.well-known".Equals(d.Path));
             if (wellKnownDir == null)
             {
                 Verbose("creating virtual directory /.well-known/acme");
-                var wellKnownVdir = root.VirtualDirectories.Add("/.well-known", wellKnownPath);                
-                var acmeVdir = root.VirtualDirectories.Add("/.well-known/acme", acmePath);
+                var wellKnownVdir = root.VirtualDirectories.Add("/.well-known", wellKnownPath);
+                var acmeVdir = root.VirtualDirectories.Add("/.well-known/acme", Path.Combine(wellKnownPath, "acme"));
                 manager.CommitChanges();
-            }           
-            
-             
+            }
         }
 
-        private static void CreateDirectory(string challengePath)
+        private static string CreateWellKnownDirectory(string token, string challengeJson)
         {
-            if (!System.IO.Directory.Exists(challengePath))
-            {
-                Verbose($"creating directory {challengePath}");
-                System.IO.Directory.CreateDirectory(challengePath);
-            }
+            var challengePath = Path.Combine(Path.GetTempPath(), "oocx.acme");
+            CreateDirectory(challengePath);
+            var wellKnownPath = Path.Combine(challengePath, ".well-known");
+            if (CreateDirectory(wellKnownPath)) AllowReadPermissionsForEveryone(wellKnownPath); ;
+            var acmePath = Path.Combine(wellKnownPath, "acme");
+            CreateDirectory(acmePath);
+            var challengeFilePath = Path.Combine(acmePath, token);
+            File.WriteAllText(challengeFilePath, challengeJson);
+            var acmeWebConfigFilePath = Path.Combine(acmePath, "web.config");
+            File.WriteAllText(acmeWebConfigFilePath, AcmeWebConfigContents);
+            return wellKnownPath;
+        }
+
+        private static bool CreateDirectory(string challengePath)
+        {
+            if (Directory.Exists(challengePath)) return false;
+
+            Verbose($"creating directory {challengePath}");
+            System.IO.Directory.CreateDirectory(challengePath);
+            return true;
         }
     }
 }
