8c29452d1404697afd7bb2d5c1609ecd571845da
Mathias Raacke <mathias@raacke.info>
2015-11-22 14:17:23 +0100

    removed version output, as this is already provided by CommandLineParser

--- src/Oocx.ACME.CLRConsole/Program.cs
+++ src/Oocx.ACME.CLRConsole/Program.cs
@@ -3,8 +3,7 @@
     class Program
     {
         static void Main(string[] args)
-        {
-            System.Console.WriteLine($"acme.exe v{typeof(Program).Assembly.GetName()?.Version}");
+        {            
             var program = new Oocx.ACME.Console.Program();
             program.Main(args);
         }
